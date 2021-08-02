c4a66428a02041f41f066b9ca0e046305b02be81
Sherzodbek Ortikjonov <hhll69241@gmail.com>
2021-08-02 18:11:33 +0300

    Add basic AES functionality

--- lib/en_de_cryptor.dart
+++ lib/en_de_cryptor.dart
@@ -1,16 +1,39 @@
+import 'dart:io';
+import 'dart:convert';
+
 import 'package:encrypt/encrypt.dart';
 
 void hello() {
-  final plainText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit';
-  final key = Key.fromUtf8('my 32 length key................');
-  final iv = IV.fromLength(16);
-
-  final encrypter = Encrypter(AES(key));
-
+  String keyString = getKey();
+  String plainText = getText();
+  final Key key = Key.fromUtf8(keyString);
+  final iv = IV.fromSecureRandom(16); // has size equal to AES single block size
+  final encrypter = Encrypter(AES(key, mode: AESMode.cbc));
   final encrypted = encrypter.encrypt(plainText, iv: iv);
-  final decrypted = encrypter.decrypt(encrypted, iv: iv);
+  final encrData = '${iv.base64}${encrypted.base64}';
+  final encrIv = encrData.substring(
+      0, 24); // 24 because base64 uses padding, TO DO: read about it
+  final encrPayload = encrData.substring(24);
 
-  print(decrypted); // Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  print(encrypted
-      .base64); // R4PxiU3h8YoIRqVowBXm36ZcCeNeZ4s1OvVBTfFlZRdmohQqOpPQqD1YecJeZMAop/hZ4OxqgC1WtwvX/hP9mw==
+  print(encrIv);
+  print(encrPayload);
+
+  final decrPayload = encrypter.decrypt64(
+    base64.normalize(encrPayload),
+    iv: IV.fromBase64(encrIv),
+  );
+
+  print(decrPayload);
+}
+
+String getKey() {
+  print("Enter key:");
+  stdin.echoMode = false;
+  return stdin.readLineSync();
+}
+
+String getText() {
+  print("Enter text:");
+  stdin.echoMode = true;
+  return stdin.readLineSync();
 }
